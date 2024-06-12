b4f57ad088c3b9e48ff5299303631efa714ac98b
Asad <asadullahkhan0098@gmail.com>
2024-06-13 00:42:03 +0530

    Write a function for adding book to the library

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addBtn = document.querySelector('.add-btn-container>button');
+const modal = document.querySelector('dialog');
 const library = document.querySelector('.library');
 
 const myLibrary = [];
@@ -8,4 +9,10 @@ function Book(author, title, pages, isRead) {
     this.title = title;
     this.pages = pages;
     this.isRead = isRead;
+}
+
+function addBookToLibrary(inputs) {
+    let [author, title, pages, isRead] = inputs;
+    const newBook = new Book(author, title, pages, isRead);
+    myLibrary.push(newBook);
 }
