c314ed16c11a51e60d65f8df1f59034673c595b1
Asad <asadullahkhan0098@gmail.com>
2024-06-13 12:43:07 +0530

    Rename the setIsRead to toggleIsRead & refactor it

--- script.js
+++ script.js
@@ -19,4 +19,25 @@ function addBookToLibrary(inputs) {
     let [author, title, pages, isRead] = inputs;
     const newBook = new Book(author, title, pages, isRead);
     myLibrary.push(newBook);
+    let tHeadings = ['Author', 'Title', 'Pages'];
+    const card = document.createElement('div');
+    const table = document.createElement('table');
+    const trs = [];
+    for(let i = 0; i < 3; i++) {
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const td = document.createElement('td');
+        th.textContent = tHeadings[i];
+        td.textContent = inputs[i];
+        tr.appendChild(th);
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
+    const bookBtnsContainer = document.createElement('div');
+    bookBtnsContainer.classList.add('book-btns');
+    const toggleReadBtn = document.createElement('button');
+    const removeBtn = document.createElement('button')
+    card.appendChild(table);
+    library.appendChild(card);
+    library.style.display = 'flex';
 }
