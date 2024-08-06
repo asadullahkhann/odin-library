233947fe5697615c61f6a26e6a6a4471cba7d55b
Asad <asadullahkhan0098@gmail.com>
2024-08-06 15:26:32 +0530

    Use classes instead of plain constructors

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const library = document.querySelector('.library');
 
 const myLibrary = [];
 
-function Book(author, title, pages, isRead) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.isRead = isRead;
-}
-
-Book.prototype.toggleIsRead = function() {
-    this.isRead = this.isRead === true ? false : true;
+class Book {
+    constructor(author, title, pages, isRead){
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+    toggleIsRead() {    
+        this.isRead = this.isRead === true ? false : true;
+    }
 }
 
 function addBookToLibrary() {
